406827a1d02a22b2c15496e88b814d580b929c33
Dmitriy Samoilov <e2718281828@ya.ru>
2011-03-11 15:49:02 +0200

    chaged can_move and move functions so now they could accept 'to_pos' option along with 'to' direction

--- shape.js
+++ shape.js
@@ -7,27 +7,39 @@ function Shape(ctx, a_current_player_id) {
   var shape_x_shift = (current_player_id - 1) * CANVAS_WIDTH - (current_player_id - 1) * SHAPE_WIDTH; // 1st player - at left, 2nd - at right
 
   this.move = function(options) {
-    if(options['to'] == 'bottom') {
-      current_y_position += 5;
-    } else if(options['to'] == 'top') {
-      current_y_position -= 5;
+    if(options['to_pos']) {
+      current_y_position = options['to_pos'];
+    } else if(options['to']) {
+      if(options['to'] == 'bottom') {
+        current_y_position += 5;
+      } else if(options['to'] == 'top') {
+        current_y_position -= 5;
+      }
     }
   }
 
   this.can_move = function(options) {
-    if(options['to'] == 'bottom') {
-      if((current_y_position + SHAPE_HEIGHT) > CANVAS_HEIGHT) {
+    if(options['to_pos']) {
+      if(((options['to_pos'] + SHAPE_HEIGHT) > CANVAS_HEIGHT) || (options['to_pos'] <= 0)) {
         return false;
       } else {
         return true;
       }
-    } else if(options['to'] == 'top') {
-      if(current_y_position <= 0) {
-        return false;
-      } else {
-        return true;
+    } else if(options['to']) {
+      if(options['to'] == 'bottom') {
+        if((current_y_position + SHAPE_HEIGHT) > CANVAS_HEIGHT) {
+          return false;
+        } else {
+          return true;
+        }
+      } else if(options['to'] == 'top') {
+        if(current_y_position <= 0) {
+          return false;
+        } else {
+          return true;
+        }
       }
-    }      
+    }
   }
 
   this.draw = function() {    
